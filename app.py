c5d4fdda672718ce423ee240b27a49c3bcecc17a
Rishabh <joshirishabh631@gmail.com>
2026-03-02 13:10:51 +0530

    upgrade app UI

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
 from flask import Flask
+import socket
+import datetime
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Kubernetes "
+    hostname = socket.gethostname()
+    time = datetime.datetime.now()
+    return f"""
+    <h1>🚀 CI/CD SUCCESS</h1>
+    <p><b>Pod Hostname:</b> {hostname}</p>
+    <p><b>Deployed At:</b> {time}</p>
+    <p><b>Status:</b> Running via ArgoCD + GitHub Actions</p>
+    """
 
-app.run(host="0.0.0.0", port=5000)
+@app.route("/health")
+def health():
+    return "OK"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
